4b85dae6a9209f74d55a9efd5b74e85ee0a4846c
Allan Anderson <aanderson@nait.ca>
2022-02-18 10:33:47 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 // Find and assign your constant.
-
+const clickButton = document.querySelector('.toggle-btn');
 
 // Create a click event for it that toggles a CSS class. 
+clickButton.addEventListener('click', () => {
+  document.querySelector('nav').classList.toggle('show-nav');
+});
